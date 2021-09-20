79a6eff63200500c85c51682ddbacd5fccc30794
可 路 <1300876003@qq.com>
2021-09-20 19:06:46 +0800

    路由，用于实现单页web应用

--- vue_test/26_src_求和案例_mapState与mapGetters/components/Count.vue
+++ vue_test/26_src_求和案例_mapState与mapGetters/components/Count.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         <h1>当前求和为：{{sum}}</h1>
+        <h3>当前求和放大10倍为：{{bigSum}}</h3>
+        <h3>我在{{school}}学习{{subject}}</h3>
         <select v-model.number="n">
             <option value="1">1</option>
             <option value="2">2</option>
@@ -14,32 +16,33 @@
 </template>
 
 <script>
+    import {mapState, mapGetters} from 'vuex'
     export default {
         name:'Count',
         data() {
             return {
                 n:1,
-                sum:0
             }
+        },
+        computed:{
+            // 借助mapState生成计算属性，从state中读取数据
+            ...mapState(['sum', 'school', 'subject']),
+            ...mapGetters(['bigSum'])
         },
         methods:{
             increment(){
-                this.sum += this.n
+                this.$store.commit('JIA',this.n)
             },
             decrement(){
-                this.sum -= this.n
+                this.$store.commit('JIAN',this.n)
             },
             incrementOdd(){
-                if (this.sum % 2){
-                    this.sum += this.n
-                }
+                this.$store.dispatch('jiaOdd',this.n)
             },
             incrementWait(){
-                setTimeout(() => {
-                    this.sum += this.n
-                }, 500);
+                this.$store.dispatch('jiaWait',this.n)
             }
-        }
+        },
     }
 </script>
 
